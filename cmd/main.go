6d34ffddd1723933742fa4f4ee5adccf1d52b29c
Iftikhor99 <1987-2005@mail.ru>
2020-11-16 20:02:30 +0500

    new changes 1

---
+++ cmd/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net"
+	"net/http"
+	"os"
+
+	"github.com/Iftikhor99/server/cmd/app"
+	"github.com/Iftikhor99/server/pkg/banners"
+)
+
+func main() {
+	host := "0.0.0.0"
+	port := "9999"
+
+	if err := execute(host, port); err != nil {
+		os.Exit(1)
+	}
+
+}
+
+func execute(host string, port string) (err error) {
+	mux := http.NewServeMux()
+	bannersSvc := banners.NewService()
+	server := app.NewServer(mux, bannersSvc)
+	server.Init()
+	//	bannersSvc.Initial()
+	srv := &http.Server{
+		Addr:    net.JoinHostPort(host, port),
+		Handler: server,
+	}
+
+	return srv.ListenAndServe()
+}
